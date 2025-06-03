a757ba973f8a319507f2596474a3fb33239bf54b
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-03 18:32:20 +0000

    new commit

---
+++ backend/app.ts
@@ -0,0 +1,40 @@
+
+import express from 'express';
+const app =  express();
+const port: number = process.env.PORT || 8080 ; 
+import fs from 'fs';
+
+import path from 'path';
+
+
+
+// app.get("/home" ,(req, res)=>{
+//     const file= path.join(__dirname, './content/about.txt')
+//     fs.readFile(file , 'utf8', (err,data) =>{
+//         if (err) {
+//             console.error('error parsing the file');
+            
+//         } else {
+//             res.send(data)
+//         }
+//     })
+
+// });
+// app.use(express.static(path.join(__dirname, "../Client/build")));
+// app.get("/*" , function (req, res){
+
+//     res.sendFile(
+//         path.join(__dirname, "../Client/build/index.html"),
+//         (err)=>{
+//             if(err){
+//                 res.status(500).send(err)
+//             }
+//         }
+//     );
+
+// });
+
+
+app.listen(port, ()=>{
+    console.log(`server is running on ${port}`)
+})
