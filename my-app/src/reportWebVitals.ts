70fc9e9d5b706431537e16e23223f2b462788456
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-05 19:06:50 -0500

    new commit

--- my-app/src/reportWebVitals.ts
+++ my-app/src/reportWebVitals.ts
@@ -1,4 +1,6 @@
-import { ReportHandler } from 'web-vitals';
+import type { Metric } from 'web-vitals';
+
+type ReportHandler = (metric: Metric) => void;
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
