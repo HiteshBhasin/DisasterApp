8adc4a75e6aa5993256bbc173e67ca9c0fc3edda
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-05 19:43:37 -0500

    new commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,19 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <title> Creating a map with leaftlet and maptiler</title>
       <header className="App-header">
        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
       </header>
     </div>
   );
