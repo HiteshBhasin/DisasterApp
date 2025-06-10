a79696b55a3d9330b695baf68d5a8a3480b12b6a
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-10 14:48:46 +0000

    new commit

--- myapp/src/layers.jsx
+++ myapp/src/layers.jsx
@@ -2,4 +2,28 @@ import {useState, useEffect} from "react";
 import L from 'leaflet';
 import { Marker, Popup, useMapEvent, useMap} from 'react-leaflet';
 
-nasaData = "https://firms.modaps.eosdis.nasa.gov/api/area/json/<adde6368823b1c811de264a026f39f29>/MODIS_NRT/NorthAmerica/24h";
+nasaApi = "https://firms.modaps.eosdis.nasa.gov/api/area/json/<adde6368823b1c811de264a026f39f29>/MODIS_NRT/NorthAmerica/24h";
+
+function layerReturn(params) {
+    const [fireData, getData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const data = await fetch(nasaApi);
+                if(!data){
+                    console.log("no data came through");
+                }
+                const jsonData = await data.json();
+                console.log(jsonData);
+
+            } catch (error) {
+                 console.error(error.message);
+            }
+
+            
+
+        }
+        fetchData();
+    }, []);
+}
