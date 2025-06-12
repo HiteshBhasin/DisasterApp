c9b04d43cc229073ce2f13c048f6ba42b9b8c199
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-12 13:21:20 -0500

    new commit

--- myapp/src/layers.jsx
+++ myapp/src/layers.jsx
@@ -1,21 +1,33 @@
 import {useState, useEffect} from "react";
 import L from 'leaflet';
-import { Marker, Popup, useMapEvent, useMap, LayersControl, WMSTileLayer, TileLayer, LayerGroup} from 'react-leaflet';
+import { Marker, Popup, LayersControl, TileLayer, LayerGroup} from 'react-leaflet';
 
-const nasaApi = "https://firms.modaps.eosdis.nasa.gov/api/area/json/<adde6368823b1c811de264a026f39f29>/MODIS_NRT/NorthAmerica/24h";
+const nasaApi = "https://firms.modaps.eosdis.nasa.gov/api/area/json/a1531693fe55b8fce18f80c7f1417972/MODIS_NRT/NorthAmerica/24h";
 
-function LayerReturn(params) {
+function LayerReturn() {
+       const fireIcon = L.icon({
+         iconUrl: 'https://cdn-icons-png.flaticon.com/128/10760/10760625.png',
+         iconSize: [30,30],
+         iconAnchor: [15,30],
+        popupAnchor:[0,-30],
+    
+       
+       }); 
     const [fireData, getData] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const data = await fetch(nasaApi);
+                console.log("FIRMS API Response Status:", data.status, data.statusText);
+                console.log("FIRMS API Response Headers:", data.headers);
+                
                 if(!data){
-                    console.log("no data came through");
+                    const errorRes = await data.text();
+                    console.log(`no data came through ${errorRes}`);
                 }
                 const jsonData = await data.json();
-                console.log(jsonData);
+                console.log(jsonData, "jsondata");
                 var newData = [];
                 if (jsonData && jsonData.length>0){
                     for (const jData of jsonData ){
@@ -28,7 +40,7 @@ function LayerReturn(params) {
                 }
                 getData(newData);
             } catch (error) {
-                 console.error(error.message);
+                 console.log(error.message, "data not coming");
             }
         }
         fetchData();
@@ -42,14 +54,14 @@ function LayerReturn(params) {
 
         <LayersControl.Overlay name = "fires in Canada">
             <LayerGroup>
-                {fireData.map((fire, idx)=>{
-                    <Marker key={idx} position={[fire.lat, fire.lon]}>
+                {fireData.map((fire, idx)=>(
+                    <Marker key={idx} position={[fire.lat, fire.lon]} icon={fireIcon}>
                         <Popup>
                             Date: {fire.date}<br />
                             Time: {fire.time}<br />
                         </Popup>
                     </Marker>
-                })}
+                ))}
             </LayerGroup>
         </LayersControl.Overlay>
     );
