be197976c977f767a3c9d84669808962f81c6c7c
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-12 19:53:51 -0500

    new commit

---
+++ myapp/src/resources.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+function Resources() {
+    return(
+        <div id="evacueesResources">
+            <ul id="resourceList">
+                <li>
+                    <a href="https://www.gov.mb.ca/wildfire/evacuees.html">Resources For Evacuees</a>
+                </li>
+                <li>
+                    <a href="https://www.gov.mb.ca/wildfire/news.html"> Updates</a>
+                </li>
+                <li>
+                    <a href="https://www.gov.mb.ca/wildfire/faq.html">FAQs</a>
+                </li>
+                <li>
+                    <a href="https://www.gov.mb.ca/wildfire/contact.html">Contact Authorities</a>
+                </li>
+            </ul>
+        </div>
+    );
+}
+export default Resources;
