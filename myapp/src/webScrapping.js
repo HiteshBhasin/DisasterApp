4fdc18647b5223bf7fe4e2df2fa5a1ff940f9b44
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-15 12:52:14 +0000

    new commit

--- myapp/src/webScrapping.js
+++ myapp/src/webScrapping.js
@@ -1,11 +1,13 @@
 import React ,{useState, useEffect} from "react";
-
+import puppeteer from "puppeteer";
 const WebbData = ()=>{
     const [data, getData] = useState(" ");
 
     useEffect(()=>
     {
-        
+        async function News() {
+            
+        }
     });
 
 
