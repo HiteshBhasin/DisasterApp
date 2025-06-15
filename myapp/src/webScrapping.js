99899deea4bae8c50d2213de502b0fdb4c06d61f
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-15 14:26:20 +0000

    new commit

--- myapp/src/webScrapping.js
+++ myapp/src/webScrapping.js
@@ -1,13 +1,21 @@
 import React ,{useState, useEffect} from "react";
-import puppeteer from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
 const WebbData = ()=>{
     const [data, getData] = useState(" ");
 
     useEffect(()=>
     {
         async function News() {
-            
-        }
+            const browser = await puppeteer.launch({headless:false, defaultViewport: null});
+            const page = await browser.newPage();
+
+            await page.goto("https://www.gov.nt.ca/en/public-safety/latest-updates",{waitUntil:"domcontentloaded"});
+            const news = await page.evaluate(()=>{
+
+                const updates = 
+            });
+        }; 
+        
     });
 
 
