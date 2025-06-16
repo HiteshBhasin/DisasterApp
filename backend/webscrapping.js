4f84b99806c822780c21a08c0994bf8267b7d532
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-16 15:50:10 +0000

    new commit

--- backend/webscrapping.js
+++ backend/webscrapping.js
@@ -1,2 +1,31 @@
 import puppeteer from "puppeteer";
 
+async function WebScrapping() {
+    const browser = await puppeteer.launch({
+    headless: "true", //A headless browser is essentially a browser without a graphical user interface, which allows for faster and more efficient automation.
+    defaultViewport: null
+});
+
+const page = await browser.newPage();
+
+await page.goto("https://www.manitoba.ca/wildfire/news.html",
+    {waitUntil: "domcontentloaded",});
+
+try {
+    const contents = await page.evaluate(()=>{
+    const content = document.querySelectorAll(".col-3-4");
+    if (content){
+    for(let i =0;i<content.length;i++){
+        console.log(content[i]);
+    }
+    
+    
+    }
+});
+} catch (error) {
+    console.log(error.message);
+}
+ 
+
+
+}
