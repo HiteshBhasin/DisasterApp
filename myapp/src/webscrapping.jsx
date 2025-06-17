faacd1c42ad201c338c5e3a5e807292415c43efc
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2025-06-17 14:02:25 +0000

    new commit

--- myapp/src/webscrapping.jsx
+++ myapp/src/webscrapping.jsx
@@ -1,1 +1,25 @@
 import  { useState, useEffect } from "react";
+
+function Updates() {
+    const [updatedData, getUpdatedData] = useState(" ");
+
+    useEffect(()=>{
+
+        async function getUpdates( ) {
+            try {
+                url="";
+                const fetchData =fetch(url);
+                if(fetchData){
+                const jsonData = (await fetchData).json()}
+                else{
+                    console.error("We didnt get any Data", jsonData);
+                }
+            } catch (error) {
+                
+            }
+        }
+
+
+
+    });
+}
