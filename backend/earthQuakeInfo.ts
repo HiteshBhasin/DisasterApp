8074ac2307337d3492b356f51cab8bab2bc3e75f
Hitesh Bhasin <77706673+HiteshBhasin@users.noreply.github.com>
2026-08-08 11:30:18 -0500

    time fixed

--- backend/earthQuakeInfo.ts
+++ backend/earthQuakeInfo.ts
@@ -1,12 +1,11 @@
-import * as cheerio from 'cheerio';
-
 const HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
 };
 
-async function EarcthQuakeInfo(NOW:time) {
-
-const EQK_URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime={NOW-1min}&endtime={NOW}";
+async function EarcthQuakeInfo(NOW: Date) {
+    const startTime = new Date(NOW.getTime() - 60000).toISOString();
+    const endTime = NOW.toISOString();
+    const EQK_URL = `https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=${startTime}&endtime=${endTime}`;
 
 try{
 const data = await fetch(EQK_URL, { headers: HEADERS }).then(res => res.json());
